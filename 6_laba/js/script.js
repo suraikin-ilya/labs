82139a50c3fe5a6a1a8f0e08e6e8dbbb20697c51
suraikin-ilya <ilya.suraikin@mail.ru>
2020-12-19 17:36:02 +0300

    Update script.js

---
+++ 6_laba/js/script.js
@@ -0,0 +1,73 @@
+var h1Id=document.getElementById('h1');
+var buttonId=document.getElementById('button');
+var counter=0;
+buttonId.addEventListener('click',function(){
+    counter=counter+50;
+    h1Id.textContent='Очки:'+counter;
+});
+
+$(document).ready(function () {
+    $('.fish__1').click(function (event) {
+      $('.fish__1').toggleClass('popadanie');
+    });
+});
+
+$(document).ready(function () {
+    $('.fish__2').click(function (event) {
+      $('.fish__2').toggleClass('popadanie');
+    });
+});
+
+$(document).ready(function () {
+    $('.fish__3').click(function (event) {
+      $('.fish__3').toggleClass('popadanie');
+    });
+});
+
+$(document).ready(function () {
+    $('.fish__4').click(function (event) {
+      $('.fish__4').toggleClass('popadanie');
+    });
+});
+
+$(document).ready(function () {
+    $('.fish__5').click(function (event) {
+      $('.fish__5').toggleClass('popadanie');
+    });
+});
+
+
+$(document).ready(function () {
+    $('.fish__6').click(function (event) {
+      $('.fish__6').toggleClass('popadanie');
+    });
+});
+
+
+$(document).ready(function () {
+    $('.fish__7').click(function (event) {
+      $('.fish__7').toggleClass('popadanie');
+    });
+});
+
+
+$(document).ready(function () {
+    $('.fish__8').click(function (event) {
+      $('.fish__8').toggleClass('popadanie');
+    });
+});
+
+
+$(document).ready(function () {
+    $('.fish__9').click(function (event) {
+      $('.fish__9').toggleClass('popadanie');
+    });
+});
+
+
+$(document).ready(function () {
+    $('.fish__10').click(function (event) {
+      $('.fish__10').toggleClass('popadanie');
+    });
+});
+
